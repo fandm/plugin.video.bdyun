a40d9f8aca7dc53ddd1dc5956ad39982f5c07dc4
Andrii Severynenko <andriy.severynenko@ffwagency.com>
2017-11-21 22:28:13 +0200

    Translation test.

--- resources/modules/get_auth.py
+++ resources/modules/get_auth.py
@@ -18,10 +18,10 @@ class VcodeWindow(xbmcgui.WindowDialog):
         # windowItems
         self.image = xbmcgui.ControlImage(80, 100, 500, 200, self.vcode_path)
         self.buttonInput = xbmcgui.ControlButton(
-            100, 330, 220, 50, label=u'输入验证码', alignment=6, font='font13', textColor='0xFFFFFFFF'
+            100, 330, 220, 50, label=u'Ввести код', alignment=6, font='font13', textColor='0xFFFFFFFF'
         )
         self.buttonRefresh = xbmcgui.ControlButton(
-            290, 330, 220, 50, label=u'刷新验证码', alignment=6, font='font13', textColor='0xFFFFFFFF'
+            290, 330, 220, 50, label=u'Обновить код', alignment=6, font='font13', textColor='0xFFFFFFFF'
         )
         self.addControls([self.image, self.buttonInput, self.buttonRefresh])
         self.buttonInput.controlRight(self.buttonRefresh)
@@ -39,7 +39,7 @@ class VcodeWindow(xbmcgui.WindowDialog):
                 self.image = xbmcgui.ControlImage(80, 100, 500, 200, self.vcode_path)
                 self.addControl(self.image)
             else:
-                dialog.ok('Error', u'无法刷新验证码，请重试')
+                dialog.ok('Error', u'Не удалось обновить код, повторите попытку')
 
 
 
@@ -64,7 +64,7 @@ def run(username,password):
         win.doModal()
         codeString = win.codeString
 
-        verifycode = dialog.input(heading=u'验证码')
+        verifycode = dialog.input(heading=u'Код')
         if verifycode:
             err_no,query = auth.post_login(cookie,tokens,username,password_enc,rsakey,verifycode,codeString)
             if err_no == 0:
@@ -75,18 +75,18 @@ def run(username,password):
                     return auth_cookie,tokens
 
             elif err_no == 4:
-                dialog.ok('Error',u'密码错误')
+                dialog.ok('Error',u'Неверный пароль')
 
             elif err_no == 6:
-                dialog.ok('Error',u'验证码错误')
+                dialog.ok('Error',u'Ошибка проверки кода')
 
             else:
-                dialog.ok('Error',u'未知错误，请重试')
+                dialog.ok('Error',u'Неизвестная ошибка, повторите попытку')
         else:
-            dialog.ok('Error',u'请输入验证码')
+            dialog.ok('Error',u'Введите код')
     
     elif err_no == 4:
-        dialog.ok('Error',u'密码错误')
+        dialog.ok('Error',u'Неверный пароль')
 
     elif err_no == 0:
         auth_cookie = query
@@ -96,6 +96,6 @@ def run(username,password):
             return auth_cookie,tokens
 
     else:
-        dialog.ok('Error',u'未知错误，请重试')
+        dialog.ok('Error',u'Неизвестная ошибка, повторите попытку')
     
     return None,None
